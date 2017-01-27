0e187e926142dd03097b26c2628a49f1477e1d2a
aishwarya <lanetteam.milan@gmail.com>
2017-01-27 17:44:30 +0530

    contact us plugin added with database connectivity

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -1,6 +1,16 @@
 <?php
+//this goes in functions.php near the top
+function my_scripts_method() {
+// register your script location, dependencies and version
+   wp_register_script('custom_script',
+   get_template_directory_uri() . '/js/jquery-3.1.1.js',
+   array('jquery'),
+   '1.0' );
+ // enqueue the script
+  wp_enqueue_script('custom_script');
+  }
+add_action('wp_enqueue_scripts', 'my_scripts_method');
 // Our custom post type function
-
 add_action( 'init', 'create_cars_taxonomy' );
 
 function create_cars_taxonomy() {
@@ -16,7 +26,6 @@ function create_cars_taxonomy() {
 		)
 	);
 }
-
 
 function movie_reviews_init() {
     $args = array(
@@ -206,7 +215,6 @@ function prowp_twitter($atts, $content = null) {
 	}elseif ( $person == 'hal' ) {
 		return '<a href="http://twitter.com/freeholdhal">@freeholdhal</a>';
 	}
-	//return '<a href="http://twitter.com/williamsba">@williamsba</a>';
 }
 
 add_filter( 'the_content', 'prowp_profanity_filter' );
@@ -218,11 +226,8 @@ return $content;
 
 add_action( 'comment_text', 'prowp_email_new_comment' );
 function prowp_email_new_comment($content) {
- //$content="This is Comment: ".$content;
  return $content;
 }
-
-
 
 add_filter ( 'the_content', 'prowp_subscriber_footer' );
 function prowp_subscriber_footer( $content ) {
@@ -234,5 +239,91 @@ if( is_single() ) {
 	return $content;
 }
 
+// Custom widget added
+class my_plugin extends WP_Widget {
 
+// constructor
+function my_plugin() {
+// Give widget name here
+parent::__construct(false, $name = __('My Widget', 'wp_widget_plugin') );
+
+}
+// widget form creation
+
+function form($instance) {
+
+// Check values
+if( $instance) {
+    $title = esc_attr($instance['title']);
+    $type = $instance['type'];
+    $textarea = $instance['textarea'];
+  } else {
+    $title = '';
+    $textarea = '';
+    $type = '';
+}
+
+?>
+<p>
+<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+</p>
+<p>
+<label for="<?php echo $this->get_field_id('type'); ?>"><?php _e('Type', 'wp_widget_plugin'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('type'); ?>" name="<?php echo $this->get_field_name('type'); ?>" type="text" value="<?php echo $type; ?>" />
+</p>
+<p>
+<label for="<?php echo $this->get_field_id('textarea'); ?>"><?php _e('Description:', 'wp_widget_plugin'); ?></label>
+<textarea class="widefat" id="<?php echo $this->get_field_id('textarea'); ?>" name="<?php echo $this->get_field_name('textarea'); ?>" rows="7" cols="20" ><?php echo $textarea; ?></textarea>
+</p>
+
+<?php
+}
+
+
+function update($new_instance, $old_instance) {
+$instance = $old_instance;
+// Fields
+$instance['title'] = strip_tags($new_instance['title']);
+$instance['type'] = strip_tags($new_instance['type']);
+$instance['textarea'] = strip_tags($new_instance['textarea']);
+return $instance;
+}
+
+// display widget
+function widget($args, $instance) {
+extract( $args );
+
+// these are the widget options
+$title = apply_filters('widget_title', $instance['title']);
+$type = $instance['type'];
+$textarea = $instance['textarea'];
+echo $before_widget;
+
+// Display the widget
+echo '<div class="widget-text wp_widget_plugin_box" style="width:269px; padding:5px 9px 20px 5px; border: 3px solid rgb(231, 15, 52); background: #DAF7A6  ; border-radius: 35px; margin: 10px 0 25px 0;">';
+echo '<div class="widget-title" style="width: 90%; height:30px; margin-left:3%;font-size:20px; ">';
+
+// Check if title is set
+if ( $title ) {
+echo $before_title . $title . $after_title ;
+}
+echo '</div>';
+
+// Check if textarea is set
+echo '<div class="widget-textarea" style="width: 90%; margin-left:3%; padding:5px; background-color: white; border-radius: 3px; min-height: 70px;">';
+echo '<div class="widget-type" style="width: 90%; margin-left:3%; padding:3px; background-color: white; border-radius: 3px; min-height: 70px;">';
+if( $type) {
+echo '<p class="wp_widget_plugin_type" style="font-size:15px;border: 2px solid black;padding-left:8px;font-size:18 em;background-color:#FFA07A"><b>'.$type.'</b></p>';
+}
+if( $textarea ) {
+echo '<p class="wp_widget_plugin_textarea" style="font-size:15px;">'.$textarea.'</p>';
+}
+echo '</div>';
+echo '</div>';
+echo $after_widget;
+}
+}
+// register widget
+add_action('widgets_init', create_function('', 'return register_widget("my_plugin");'));
 
